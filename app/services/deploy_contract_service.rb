7d0497cdbd6edd8b9a2e92023053e3400fa71f24
Артем <artem@melchakov>
2018-07-11 13:23:52 +0300

    Fix deploy contract service

--- app/services/deploy_contract_service.rb
+++ app/services/deploy_contract_service.rb
@@ -11,9 +11,9 @@ class DeployContractService
       referrer = Owner.by_profile(profile_id)
       referral = Owner.by_profile(referrer_profile_id)
       referrer_address = referrer.root ? referrer.ethereum_wallet.address : referrer.contract_address
-      
+
       @contract.deploy_and_wait(Settings.http_token_address, referral.ethereum_wallet.address, referrer_address)
-      referral.update(referrer_id: referrer.id, contract_address: @contract.address)
+      referral.update(referrer_id: referrer.id, contract_address: @contract.address) if @contract.address
     end
 
 
