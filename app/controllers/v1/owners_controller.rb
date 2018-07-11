d86852f619620b682405c93333df847a22fb1258
Артем <artem@melchakov>
2018-07-11 16:45:45 +0300

    Add spec for root invite

--- app/controllers/v1/owners_controller.rb
+++ app/controllers/v1/owners_controller.rb
@@ -1,6 +1,6 @@
 class V1::OwnersController < ApplicationController
   def create
-  	respond_with WalletService.create(params[:profile_id], "Owner"), serializer: V1::ProfileSerializer, 
+  	respond_with WalletService.create(params[:profile_id], "Owner", params[:profile_params]), serializer: V1::ProfileSerializer, 
   				       location: v1_advertisers_path
   end
 
--- spec/requests/v1/owners/create_spec.rb
+++ spec/requests/v1/owners/create_spec.rb
@@ -1,29 +1,34 @@
 require "rails_helper"
 
 RSpec.describe "Wallet create" do
-  context "Owner" do
+  context "Root invite" do
     let(:profile_id) { rand(1..100) }
 
     context "with valid id" do
       it "returns status :created" do
-        post v1_owners_path, params: { profile_id: profile_id }
+        post v1_owners_path, params: { profile_id: profile_id, profile_params: { root: true } }
         expect(response).to have_http_status(:created)
       end 
 
        it "creates new ethereum wallet" do
          expect do
-           post v1_owners_path, params: { profile_id: profile_id }
+           post v1_owners_path, params: { profile_id: profile_id, profile_params: { root: true } }
          end.to change { EthereumWallet.count }.by(1)
        end
   
       it "returns correct profile_id" do
-        post v1_owners_path, params: { profile_id: profile_id }
+        post v1_owners_path, params: { profile_id: profile_id, profile_params: { root: true } }
         expect(response.body).to be_json_eql(profile_id).at_path("profile_id")
       end
 
       it "returns address" do
-        post v1_owners_path, params: { profile_id: profile_id }
+        post v1_owners_path, params: { profile_id: profile_id, profile_params: { root: true } }
         expect(response.body).to have_json_path("address")
+      end
+
+      it "doesn't create referral contract" do
+        post v1_owners_path, params: { profile_id: profile_id, profile_params: { root: true } }
+        expect(Owner.by_profile(profile_id).contract_address).to be_nil
       end
     end
 
@@ -33,18 +38,18 @@ RSpec.describe "Wallet create" do
       let!(:ethereum_wallet) { Fabricate(:ethereum_wallet, userable_id: owner.id, userable_type: "Owner") }
 
       it "returns status :unprocessable_entity" do
-        post v1_owners_path, params: { profile_id: profile_id }
+        post v1_owners_path, params: { profile_id: profile_id, profile_params: { root: true } }
         expect(response).to have_http_status(:unprocessable_entity)
       end
 
       it "doesn't create new ethereum wallet" do
         expect do
-          post v1_owners_path, params: { profile_id: profile_id }
+          post v1_owners_path, params: { profile_id: profile_id, profile_params: { root: true } }
         end.to_not change(EthereumWallet, :count)
       end
   
       it "returns errors" do
-        post v1_owners_path, params: { profile_id: profile_id }
+        post v1_owners_path, params: { profile_id: profile_id, profile_params: { root: true } }
         expect(response.body).to have_json_path("errors")
       end
     end
