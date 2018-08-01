f158c6a1acd9c227c6c73909cdf299edf0e893a1
Артем <artem@melchakov>
2018-08-01 13:06:08 +0300

    Fix wallet service

--- app/services/wallet_service.rb
+++ app/services/wallet_service.rb
@@ -40,37 +40,5 @@ class WalletService
         address = @profile.ethereum_wallet.address
         @client.eth_send_transaction({from: key.address, to: address, value: 0.01 * EthereumClient::WEI_IN_ETHER })
       end
-
-      def approve_tokens(spender)
-        private_hex = @profile.ethereum_wallet.private_hex
-        key = Eth::Key.new(priv: private_hex)
-        owner = spender[2..42]
-        data = perform_data('approve(address,uint256)', owner, TOKEN_FOR_APPROVE)
-        args = perform_args(key, data)    
-        tx = Eth::Tx.new(args)
-        tx.sign key
-        @client.eth_send_raw_transaction(tx.hex)["result"]
-      end
-
-      def perform_data(name_method, spender, amount)
-        hex =  Digest::SHA3.new(256).digest(name_method)
-        method_id = hex.unpack('H*')[0][0..7]
-        hex_amount = (amount).to_s.to_i(10).to_s(16)
-        zero_amount = 64 - hex_amount.size
-        method_id + "0" * 24 + spender + "0" * zero_amount + hex_amount
-      end
-
-      def perform_args(key, data)
-        chain_id = @client.net_version["result"].to_i
-        args = { 
-          nonce: @client.get_nonce(key.address), 
-          gas_price: @client.gas_price, 
-          gas_limit: @client.gas_limit,  
-          to: Settings.token_address, 
-          value: 0, 
-          data: data, 
-          chainId: chain_id
-          }
-      end
   end
 end
