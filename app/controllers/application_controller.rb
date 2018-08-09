7e00f33de3244699c29b8c4425736b2323ef3dd1
Артем <artem@melchakov>
2018-08-09 14:02:13 +0300

    Fix application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  #protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   respond_to :json
 
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
