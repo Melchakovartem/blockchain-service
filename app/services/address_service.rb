3e4c16ab1bc08ecf60724d5bea004b092d968812
Артем <artem@melchakov>
2018-08-29 15:38:31 +0300

    Fix address service

--- app/services/address_service.rb
+++ app/services/address_service.rb
@@ -6,11 +6,12 @@ class AddressService
 
       address = Transaction.where('t_from=? OR t_to=?', params, params)
       eth_balance = client.get_balance(params)
+      contract_db = Contract.find_by_address(params)
 
       wetoken = Contract.find_by_name("wetoken")
       contract = client.set_contract(wetoken.name, wetoken.address, wetoken.abi)
       wt_balance = contract.call.balance_of(params).to_f / 10**18
-      return address, eth_balance, wt_balance
+      return address, eth_balance, wt_balance, contract_db
     end
   end
 end
